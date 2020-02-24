e355ad2c90e7bab06a801642792e65fe791fa03b
alanhc <alanhc.tseng1999@gmail.com>
2020-02-24 15:42:02 +0800

    [feature] posenet+dino

--- sketch.js
+++ sketch.js
@@ -1,38 +1,23 @@
-function preload()
-{
+let jump;
+
+function setup() {
     
-}
-function setup()
-{
-    createCanvas(2000, 2000);
-    background(128);
-    img = loadImage('images/people.jpg');
-}
+    initPose();
 
-function draw()
-{
-    
+    jump=false;
+    //var keys = Object.keys(dino_runner);
+    //console.log(keys)
 }
 
-let img;
-
-
-function getPose(pose_result)
-{
-    image(img,0,0)
-    for(let i=0; i<17; i++) {
-        p=pose_result['keypoints'][i];
-        console.log('pose array:',p);
-     
-        let x=p.position.x;
-        let y=p.position.y;
-        let score = p.score;
-        if (score>0.5) {
-            fill(0,255,0); circle(x, y, 10);
-            textSize(25);
-            fill(255,255,0);text(p.part+" \n"+ Number((p.score).toFixed(2)),x+30,y);
+function draw() {
+    updatePose();
+    if (jump === true) {
+        if (!dino_runner.tRex.jumping && !dino_runner.tRex.ducking) {
+            dino_runner.playSound(dino_runner.soundFx.BUTTON_PRESS);
+            dino_runner.tRex.startJump(dino_runner.currentSpeed);
         }
-        
+        console.log('key pressed')
+        jump=false;
     }
 
 }
